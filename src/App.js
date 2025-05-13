a7c7a0b58eb8517f540c393fa061e5f44333d1e6
lamtriphucUTH <2251120436@ut.edu.vn>
2025-05-13 19:58:03 +0700

    route

--- src/App.js
+++ src/App.js
@@ -1,41 +1,27 @@
 import React from 'react'
-import styled from 'styled-components'
-import { useSelector, useDispatch } from 'react-redux'
-import { decrement, increment } from './redux/slides/counterSlide'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import { routes } from './routes'
+import HeaderComponent from './components/HeaderComponent/HeaderComponent'
+import DefaultComponent from './components/DefaultComponent/DefaultComponent'
 
 function App() {
-  const count = useSelector((state) => state.counter.value)
-  const dispatch = useDispatch()
-
-  const StyledButton = styled.button`
-  /* Adapt the colors based on primary prop */
-  background: ${props => props.$primary ? "#BF4F74" : "white"};
-  color: ${props => props.$primary ? "white" : "#BF4F74"};
-
-  font-size: 1em;
-  margin: 1em;
-  padding: 0.25em 1em;
-  border: 2px solid #BF4F74;
-  border-radius: 3px;
-`;
-
   return (
     <div>
-      <div>
-        <StyledButton
-          aria-label="Increment value"
-          onClick={() => dispatch(increment())}
-        >
-          Increment
-        </StyledButton>
-        <span>{count}</span>
-        <StyledButton
-          aria-label="Decrement value"
-          onClick={() => dispatch(decrement())}
-        >
-          Decrement
-        </StyledButton>
-      </div>
+      <Router>
+        <Routes>
+          {routes.map((route) => {
+            const Page = route.page
+            const Layout = route.isShowHeader ? DefaultComponent : React.Fragment
+            return (
+              < Route key={route.path} path={route.path} element={
+                <Layout>
+                  <Page />
+                </Layout>
+              } />
+            )
+          })}
+        </Routes>
+      </Router>
     </div>
   )
 }
